e1bb78417149aa8efc4f893bec93694459ba6592
Bastyen <bastien.alvez@makina-corpus.com>
2024-06-21 11:32:44 +0200

    improve ssr render

--- front-end/src/app/synthesis-interface/synthesis-interface.component.ts
+++ front-end/src/app/synthesis-interface/synthesis-interface.component.ts
@@ -1,6 +1,4 @@
-import { Platform } from '@angular/cdk/platform';
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID, inject } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,36 +12,34 @@ import { RouterLink } from '@angular/router';
   styleUrl: './synthesis-interface.component.scss',
 })
 export class SynthesisInterfaceComponent {
-  isPlatformBrowser: boolean = false;
   L: any;
   map: any;
   marker: any;
 
-  platform = inject(Platform);
-  platformId = inject(PLATFORM_ID);
-
-  ngOnInit() {
-    this.isPlatformBrowser = isPlatformBrowser(this.platformId);
-    this.initMap();
+  constructor() {
+    afterNextRender(() => {
+      this.initMap();
+    });
   }
 
+  ngOnInit() {}
+
   async initMap() {
-    if (this.isPlatformBrowser) {
-      this.L = await import('leaflet');
-      await import('leaflet.locatecontrol');
+    this.L = await import('leaflet');
+    await import('leaflet.locatecontrol');
+    this.map = this.L.default.map('map', { zoom: 4, center: [47, 2] });
 
-      this.map = this.L.map('map', { zoom: 4, center: [47, 2] });
-
-      this.L.tileLayer(
+    this.L.default
+      .tileLayer(
         'https://data.geopf.fr/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&STYLE=normal&FORMAT=image/png&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
         {
           attribution: "<a target='_blank' href='https://ign.fr/'>IGN</a>",
         },
-      ).addTo(this.map);
+      )
+      .addTo(this.map);
 
-      this.L.control
-        .locate({ setView: 'once', showPopup: false })
-        .addTo(this.map);
-    }
+    this.L.default.control
+      .locate({ setView: 'once', showPopup: false })
+      .addTo(this.map);
   }
 }
