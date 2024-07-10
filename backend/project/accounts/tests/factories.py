cf2ea5b72ae8af891bb7274ebf77e10f2f2dad68
J-E Castagnede <j.e.castagnede@gmail.com>
2024-07-10 15:10:15 +0200

    fix

--- backend/project/accounts/tests/factories.py
+++ backend/project/accounts/tests/factories.py
@@ -6,6 +6,8 @@ from ..models import User
 
 class UserFactory(factory.django.DjangoModelFactory):
     email = faker.Faker("email")
+    first_name = faker.Faker("first_name")
+    last_name = faker.Faker("last_name")
     password = factory.PostGenerationMethodCall("set_password", "password")
     is_active = True
 
