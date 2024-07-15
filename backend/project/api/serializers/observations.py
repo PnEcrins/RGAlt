cf65f635d33828c0f18981c0ed62fe53d7bf49df
J-E Castagnede <j.e.castagnede@gmail.com>
2024-07-15 15:28:35 +0200

    observation name is not required

--- backend/project/api/serializers/observations.py
+++ backend/project/api/serializers/observations.py
@@ -72,7 +72,7 @@ class MediaSerializer(serializers.ModelSerializer):
 
 class ObservationMixin(DynamicFieldsMixin, gis_serializers.GeoFeatureModelSerializer):
     source = serializers.SerializerMethodField()
-    name = serializers.CharField(source="public_name", read_only=False)
+    name = serializers.CharField(source="public_name", required=False)
     observer = serializers.SlugRelatedField("nickname", read_only=True)
 
     def get_source(self, obj):
