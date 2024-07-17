ec64b952754f631ddbb9647fd20eb0858ae47c54
J-E Castagnede <j.e.castagnede@gmail.com>
2024-07-17 14:30:35 +0200

    add filters to observation API endpoint

--- backend/project/api/filters.py
+++ backend/project/api/filters.py
@@ -1,18 +1,21 @@
 from django_filters import rest_framework as filters
-from django_filters.fields import DateRangeField
 
-from project.observations.models import Observation
+from project.observations.models import Observation, ObservationCategory
 
 
 class ObservationFilterSet(filters.FilterSet):
-    event_date = DateRangeField()
+    event_date = filters.DateFromToRangeFilter()
     fields = filters.CharFilter(
         method="filter_fields", help_text="filter fields you want to get"
     )
+    categories = filters.ModelMultipleChoiceFilter(
+        queryset=ObservationCategory.objects.all(),
+        field_name="category",
+    )
 
-    def filter_fields(self, qs):
+    def filter_fields(self, qs, *args, **kwargs):
         return qs
 
     class Meta:
         model = Observation
-        fields = ["event_date", "category", "fields"]
+        fields = ["event_date", "categories", "fields"]
