f84de1f3dd0d7436afeaf1a7022ef8c5170985d4
Bastyen <bastien.alvez@makina-corpus.com>
2024-07-18 16:36:51 +0200

    use environment variables on runtime

--- front-end/src/main.server.ts
+++ front-end/src/main.server.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import '@ngx-env/builder/runtime';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
