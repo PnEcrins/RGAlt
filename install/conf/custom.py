b0db78cf5c42e0e367df1eb2522db1a67c4e31a7
J-E Castagnede <j.e.castagnede@gmail.com>
2024-07-24 14:16:52 +0200

    fix default custom.py config

--- install/conf/custom.py
+++ install/conf/custom.py
@@ -1,25 +1,25 @@
 # uncomment and set settings here to use S3 for media and static files
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#authentication-settings
 #
-from storages.backends.s3 import S3Storage
+# from storages.backends.s3 import S3Storage
 
-
-class StaticStorage(S3Storage):
-    location = "static"
-    default_acl = "public-read"
-
-
-class MediaStorage(S3Storage):
-    default_acl = "public-read"
-
-
-STORAGES = {
-    "default": {
-        "BACKEND": "project.settings.MediaStorage",
-        "OPTIONS": {},
-    },
-    "staticfiles": {
-        "BACKEND": "project.settings.StaticStorage",
-        "OPTIONS": {},
-    },
-}
+#
+# class StaticStorage(S3Storage):
+#     location = "static"
+#     default_acl = "public-read"
+#
+#
+# class MediaStorage(S3Storage):
+#     default_acl = "public-read"
+#
+#
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "project.settings.MediaStorage",
+#         "OPTIONS": {},
+#     },
+#     "staticfiles": {
+#         "BACKEND": "project.settings.StaticStorage",
+#         "OPTIONS": {},
+#     },
+# }
