ff0adf910abd209e3ef7179c454f65e0227c4c8a
Bastyen <bastien.alvez@makina-corpus.com>
2024-10-17 10:57:09 +0200

    improve map configuration

--- front-end/src/environments/environment.development.ts
+++ front-end/src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
   baseMaps: {
+    zoom: 7,
+    center: [45.5, 6.5],
+    zoomTo: 19,
     mainMap: {
       url: 'https://data.geopf.fr/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&STYLE=normal&FORMAT=image/png&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
       attribution: '<a target="_blank" href="https://ign.fr/">IGN</a>',
