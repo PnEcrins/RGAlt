76d8b7c1279b9eff007abb11cf54efb548afdea1
Bastyen <bastien.alvez@makina-corpus.com>
2025-04-11 10:33:39 +0200

    improve observation detail

--- front-end/src/app/pages/observation-detail/observation-detail.component.ts
+++ front-end/src/app/pages/observation-detail/observation-detail.component.ts
@@ -1,18 +1,26 @@
-import { Component, inject, Input } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  CUSTOM_ELEMENTS_SCHEMA,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Observation, ObservationType } from '../../types/types';
 import { ObservationsService } from '../../services/observations.service';
 import { SettingsService } from '../../services/settings.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-observation-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './observation-detail.component.html',
   styleUrl: './observation-detail.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ObservationDetailComponent {
+export class ObservationDetailComponent implements OnInit {
   @Input() observation!: string;
   observationData!: { properties: Observation };
   observationType!: ObservationType;
